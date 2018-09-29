1d67b8f085d92b6af2c8ae02ffe9119fd4c67ae7
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-09-29 20:16:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+from pprint import pprint
 
+ingrid_num = 0
+cur_list = []
+cur_dish = ''
+cookbook = {}
+
+with open('еда.txt') as file:
+    for line in file:
+        line = line[:-1]
+        if not line:
+            continue
+        try:
+            ingrid_num = int(line)
+        except Exception:
+            if ingrid_num > 0:
+                # Ингридиент
+                ing_name, ing_number, ing_type = line.split(' | ')
+                ing_dct = {'ingridient_name': ing_name, 'quantity': ingrid_num, 'measure': ing_type}
+                cur_list.append(ing_dct)
+                ingrid_num -= 1
+                if not ingrid_num:
+                    cookbook[cur_dish] = cur_list
+                    cur_list = []
+            else:
+                # Блюдо
+                cur_dish = line
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    for dish in dishes:
+        book = cookbook[dish]
+        for ing in book:
+            ing['quantity'] *= person_count
+        print(book)
+
+
+print('Сколько придёт гостей?')
+visitors = input()
+
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], visitors)
