812e57a6fa0ad2ef44def257a9a3fe8cee4eb232
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-10-02 22:23:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-from pprint import pprint
+def create_cookbook(filename):
+    ingrid_num = 0
+    cur_list = []
+    cur_dish = ''
+    cookbook = {}
 
-ingrid_num = 0
-cur_list = []
-cur_dish = ''
-cookbook = {}
-
-with open('еда.txt') as file:
-    for line in file:
-        line = line[:-1]
-        if not line:
-            continue
-        try:
-            ingrid_num = int(line)
-        except Exception:
-            if ingrid_num > 0:
-                # Ингридиент
-                ing_name, ing_number, ing_type = line.split(' | ')
-                ing_dct = {'ingridient_name': ing_name, 'quantity': ingrid_num, 'measure': ing_type}
-                cur_list.append(ing_dct)
-                ingrid_num -= 1
-                if not ingrid_num:
-                    cookbook[cur_dish] = cur_list
-                    cur_list = []
-            else:
-                # Блюдо
-                cur_dish = line
+    with open(filename) as file:
+        for line in file:
+            line = line[:-1]
+            if not line:
+                continue
+            try:
+                ingrid_num = int(line)
+            except Exception:
+                if ingrid_num > 0:
+                    # Ингридиент
+                    ing_name, ing_number, ing_type = line.split(' | ')
+                    ing_dct = {'ingridient_name': ing_name, 'quantity': ingrid_num, 'measure': ing_type}
+                    cur_list.append(ing_dct)
+                    ingrid_num -= 1
+                    if not ingrid_num:
+                        cookbook[cur_dish] = cur_list
+                        cur_list = []
+                else:
+                    # Блюдо
+                    cur_dish = line
+    return cookbook
 
 
 def get_shop_list_by_dishes(dishes, person_count):
+    cookbook = create_cookbook('еда.txt')
     for dish in dishes:
         book = cookbook[dish]
         for ing in book:
@@ -35,8 +36,4 @@ def get_shop_list_by_dishes(dishes, person_count):
         print(book)
 
 
-print('Сколько придёт гостей?')
-visitors = input()
-
-
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], visitors)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
